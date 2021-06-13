8dbbb780c52b10481000459d5bb099d3633291cf
Luisangel2409 <85178962+Luisangel2409@users.noreply.github.com>
2021-06-12 21:56:20 -0500

    Update Video.h

--- Video.h
+++ Video.h
@@ -7,3 +7,18 @@
 using namespace std;
 
 class Video{
+ protected:
+    string ID, nombreVideo, tipoVideo, genero;
+    float calificacion;
+    int anioLanzamiento, duracion;
+  public:
+    Video();
+    Video(string, string, string, string, int, int);
+
+    void setID(string);
+    void setnombreVideo(string);
+    void settipoVideo(string);
+    void setgenero(string);
+    void setcalificacion(float);
+    void setanioLanzamiento(int);
+    void setduracion(int);
