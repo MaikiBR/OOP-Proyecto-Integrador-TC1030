972609ec2c50120e5554df1ee13b5973631c576a
Luisangel2409 <85178962+Luisangel2409@users.noreply.github.com>
2021-06-12 21:04:03 -0500

    Update Video.cpp

--- Video.cpp
+++ Video.cpp
@@ -4,3 +4,11 @@
 using namespace std;
 
 Video::Video(){
+  this->ID = "Sin-ID";
+  this->nombreVideo = "Sin nombre";
+  this->tipoVideo = "Sin tipo";
+  this->genero = "Sin genero";
+  this->calificacion = 0.0;
+  this->anioLanzamiento = 0;
+  this->duracion = 0;
+}
