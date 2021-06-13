534e2d26bcca17be11615305f82f671ef6073921
Luisangel2409 <85178962+Luisangel2409@users.noreply.github.com>
2021-06-12 21:04:28 -0500

    Update Video.cpp

--- Video.cpp
+++ Video.cpp
@@ -12,3 +12,13 @@ Video::Video(){
   this->anioLanzamiento = 0;
   this->duracion = 0;
 }
+
+Video::Video(string _ID, string _nombreVideo, string _tipoVideo, string _genero, int _anioLanzamiento, int _duracion){
+  this->ID = _ID;
+  this->nombreVideo = _nombreVideo;
+  this->tipoVideo = _tipoVideo;
+  this->genero = _genero;
+  this->calificacion = 0;
+  this->anioLanzamiento = _anioLanzamiento;
+  this->duracion = _duracion;
+}
