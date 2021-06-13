dafb4b4a303606de6e190debbedee026b53b9f3f
Luisangel2409 <85178962+Luisangel2409@users.noreply.github.com>
2021-06-12 21:59:38 -0500

    Update Video.cpp

--- Video.cpp
+++ Video.cpp
@@ -2,3 +2,23 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+
+Video::Video(){
+  this->ID = "Sin-ID";
+  this->nombreVideo = "Sin nombre";
+  this->tipoVideo = "Sin tipo";
+  this->genero = "Sin genero";
+  this->calificacion = 0.0;
+  this->anioLanzamiento = 0;
+  this->duracion = 0;
+}
+
+Video::Video(string _ID, string _nombreVideo, string _tipoVideo, string _genero, int _anioLanzamiento, int _duracion){
+  this->ID = _ID;
+  this->nombreVideo = _nombreVideo;
+  this->tipoVideo = _tipoVideo;
+  this->genero = _genero;
+  this->calificacion = 0;
+  this->anioLanzamiento = _anioLanzamiento;
+  this->duracion = _duracion;
+}
