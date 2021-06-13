4a750a657322662cda1feca93d20dfea3517eadb
Jesus2610 <85330226+Jesus2610@users.noreply.github.com>
2021-06-12 19:55:56 -0500

    Update Pelicula.cpp

--- Pelicula.cpp
+++ Pelicula.cpp
@@ -3,3 +3,9 @@
 #include "Pelicula.h"
 
 using namespace std;
+
+Pelicula::Pelicula():Video(){
+}
+
+Pelicula::Pelicula(string _ID, string _nombreVideo, string _tipoVideo, string _genero, int _anioLanzamiento, int _duracion):Video(_ID,_nombreVideo,_tipoVideo,_genero,_anioLanzamiento,_duracion){
+}
