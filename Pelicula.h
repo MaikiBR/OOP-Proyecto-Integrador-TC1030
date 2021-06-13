8e19b93c76e7c6b2baad748eb778056c8f7f5732
Jesus2610 <85330226+Jesus2610@users.noreply.github.com>
2021-06-12 21:52:28 -0500

    Create Pelicula.h

--- Pelicula.h
+++ Pelicula.h
@@ -5,3 +5,11 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+
+class Pelicula:public Video{
+  public:
+    Pelicula();
+    Pelicula(string, string, string, string, int, int);
+    void muestraDatosP();
+};
+#endif
